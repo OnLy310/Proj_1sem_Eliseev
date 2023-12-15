00a05e4acc57c4f6e0901f1f173914d51fbb7f34
OnLy310 <eliseev881@gmail.com>
2023-12-15 19:53:40 +0300

    PZ_6

--- PZ_6/PZ_6_9_2.py
+++ PZ_6/PZ_6_9_2.py
@@ -1,1 +1,13 @@
 # Дан список размера N. Найти количество участков, на которых его элементы монотонно убывают.
+
+def decrease_selection(lst):
+    count = 0
+    for i in range(len(lst)-1):
+        if lst[i] > lst[i+1]:
+            count += 1
+    return count
+
+
+my_list = [5, 4, 3, 2, 1, 6, 5, 4, 3, 2]
+result = decrease_selection(my_list)
+print(result)
