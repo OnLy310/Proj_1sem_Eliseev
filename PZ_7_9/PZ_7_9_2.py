7605b21dfa3c24798976d8e75efce87891165d6d
eliseev881@gmail.com <eliseev881@gmail.com>
2023-12-18 19:42:27 +0300

    Full up

--- PZ_7_9/PZ_7_9_2.py
+++ PZ_7_9/PZ_7_9_2.py
@@ -1,2 +1,10 @@
 # Дана строка, содержащая полное имя файла, то есть имя диска, список каталогов (путь), собственно имя и расширение.
 # Выделить из этой строки имя файла (без расширения).
+
+import os
+
+filename = "C:/Users/User/Documents/PycharmProjects/IS-22/Proj_1sem_Eliseev/PZ_7_9/PZ_7_9_2.py"
+basename = os.path.basename(filename)
+newname = os.path.splitext(basename)[0]
+
+print(newname)
