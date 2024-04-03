ae220ff5d1bc5daa2999ac7cf9f7b89e3eff5b71
OnLy310 <eliseev881@gmail.com>
2024-04-03 10:48:35 +0300

    Full

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -1,2 +1,23 @@
 # В матрице элементы второго столбца заменить элементами из одномерного динамического массива соответствующей
 # размерности.
+
+def replace_second_column(matrix, array):
+    if len(matrix) != len(array):
+        print("Размеры матрицы и массива должны быть одинаковыми")
+        return
+
+    for i in range(len(matrix)):
+        matrix[i][1] = array[i]
+
+
+# Пример использования:
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+
+array = [10, 11, 12]
+
+replace_second_column(matrix, array)
+
+for row in matrix:
+    print(row)
--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,2 +1,27 @@
 # В матрице найти среднее арифметическое положительных элементов, кратных 3.
 
+def average_of_multiples(matrix):
+    total = 0
+    count = 0
+
+    for row in matrix:
+        for num in row:
+            if num > 0 and num % 3 == 0:
+                total += num
+                count += 1
+
+    if count == 0:
+        return "В матрице нет положительных элементов, кратных 3"
+    else:
+        return total / count
+
+
+# Пример матрицы
+matrix = [
+    [1, 6, 9],
+    [12, 5, -3],
+    [-6, 8, 10]
+]
+
+result = average_of_multiples(matrix)
+print("Среднее арифметическое положительных элементов, кратных 3, в матрице:", result)
