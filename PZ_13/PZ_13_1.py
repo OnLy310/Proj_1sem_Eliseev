06a468d6c53fd11be67295ab6272f2280c73f400
OnLy310 <eliseev881@gmail.com>
2024-04-10 12:41:30 +0300

    Full

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -1,22 +1,20 @@
-# В матрице элементы второго столбца заменить элементами из одномерного динамического массива соответствующей
-# размерности.
-
-def replace_second_column(matrix, array):
+def replace_column(matrix, array):
     if len(matrix) != len(array):
-        print("Размеры матрицы и массива должны быть одинаковыми")
+        print("Размерности матрицы и массива не совпадают")
         return
 
-    for i in range(len(matrix)):
-        matrix[i][1] = array[i]
+    for row, value in zip(matrix, array):
+        row[1] = value
 
 
+# Пример использования
 matrix = [[1, 2, 3],
           [4, 5, 6],
           [7, 8, 9]]
 
 array = [10, 11, 12]
 
-replace_second_column(matrix, array)
+replace_column(matrix, array)
 
 for row in matrix:
     print(row)
