b055a9663897d800ab41a19f9bc8ad46d08c1cba
OnLy310 <eliseev881@gmail.com>
2024-04-10 12:57:16 +0300

    Full

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,26 +1,27 @@
 # В матрице найти среднее арифметическое положительных элементов, кратных 3.
 
-def average(matrix):
-    total = 0
+def multiple_of_3(x):
+    return x > 0 and x % 3 == 0
+
+
+def average_of_multiples_of_3(matrix):
+    positive_multiples_of_3 = filter(multiple_of_3, [element for row in matrix for element in row])
+
     count = 0
+    total = 0
+    for element in positive_multiples_of_3:
+        count += 1
+        total += element
 
-    for row in matrix:
-        for num in row:
-            if num > 0 and num % 3 == 0:
-                total += num
-                count += 1
-
-    if count == 0:
-        return "В матрице нет положительных элементов, кратных 3"
-    else:
-        return total / count
+    return total / count if count > 0 else "В матрице нет положительных элементов, кратных 3."
 
 
+# Пример матрицы
 matrix = [
-    [1, 6, 9],
-    [12, 5, -3],
-    [-6, 8, 10]
+    [3, 6, 9],
+    [12, 15, 18],
+    [21, -4, 5]
 ]
 
-result = average(matrix)
-print("Среднее арифметическое положительных элементов, кратных 3, в матрице:", result)
+print("Среднее арифметическое положительных элементов, кратных 3, в матрице:",
+      average_of_multiples_of_3(matrix))
