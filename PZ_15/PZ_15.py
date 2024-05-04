fade52f7e958cbbab30ea39347a9c1c500a325ef
OnLy310 <eliseev881@gmail.com>
2024-05-04 14:08:55 +0300

    Full

--- PZ_15/PZ_15.py
+++ PZ_15/PZ_15.py
@@ -1,3 +1,46 @@
 # Приложение УЧЕБНЫЙ ПЛАН для автоматизированного контроля учебной нагрузки по кафедре. Таблица Дисциплины должна
-# иметь следующую структуру записи: Код дисциплины, Наименование дисциплины, Специальность, Лекции (колич.часов),
-# Практические (колич.часов), Лабораторные (колич.часов), Форма отчетности.
+# иметь следующую структуру записи: Код дисциплины, Наименование дисциплины, Специальность, Лекции (кол. часов),
+# Практические (кол. часов), Лабораторные (кол. часов), Форма отчетности.
+
+import sqlite3
+
+conn = sqlite3.connect('учебный_план.db')
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS Дисциплины (
+                  Код INTEGER PRIMARY KEY,
+                  Наименование TEXT,
+                  Специальность TEXT,
+                  Лекции INTEGER,
+                  Практические INTEGER,
+                  Лабораторные INTEGER,
+                  Форма_отчетности TEXT)''')
+conn.commit()
+
+
+def add_discipline(code, name, speciality, lectures, practical, laboratory, reporting_form):
+    cursor.execute('''INSERT INTO Дисциплины (Код, Наименование, Специальность, Лекции, Практические, Лабораторные,
+    Форма_отчетности)
+                      VALUES (?, ?, ?, ?, ?, ?, ?)''', (code, name, speciality, lectures, practical, laboratory,
+                                                        reporting_form))
+    conn.commit()
+
+
+def find_discipline_by_code(code):
+    cursor.execute('''SELECT * FROM Дисциплины WHERE Код = ?''', (code,))
+    return cursor.fetchall()
+
+
+def find_discipline_by_name(name):
+    cursor.execute('''SELECT * FROM Дисциплины WHERE Наименование = ?''', (name,))
+    return cursor.fetchall()
+
+# Поиск по другим полям и для удаления и редактирования записей
+
+
+add_discipline(1, 'Математика', 'Математика', 20, 30, 10, 'Экзамен')
+result = find_discipline_by_code(1)
+print(result)
+
+# Закрытие соединения с базой данных
+conn.close()
