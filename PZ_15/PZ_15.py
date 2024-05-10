1361ba0721dd9f97a81d49bcc368fc0ca443a0b5
OnLy310 <eliseev881@gmail.com>
2024-05-10 11:27:00 +0300

    Full

--- PZ_15/PZ_15.py
+++ PZ_15/PZ_15.py
@@ -2,12 +2,11 @@
 # иметь следующую структуру записи: Код дисциплины, Наименование дисциплины, Специальность, Лекции (кол. часов),
 # Практические (кол. часов), Лабораторные (кол. часов), Форма отчетности.
 
-import sqlite3
+import sqlite3 as sq
 
-conn = sqlite3.connect('учебный_план.db')
-cursor = conn.cursor()
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS Дисциплины (
+with sq.connect('учебный_план.db') as con:
+    cursor = con.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Дисциплины (
                   Код INTEGER PRIMARY KEY,
                   Наименование TEXT,
                   Специальность TEXT,
@@ -15,7 +14,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS Дисциплины (
                   Практические INTEGER,
                   Лабораторные INTEGER,
                   Форма_отчетности TEXT)''')
-conn.commit()
+con.commit()
 
 
 def add_discipline(code, name, speciality, lectures, practical, laboratory, reporting_form):
@@ -23,7 +22,7 @@ def add_discipline(code, name, speciality, lectures, practical, laboratory, repo
     Форма_отчетности)
                       VALUES (?, ?, ?, ?, ?, ?, ?)''', (code, name, speciality, lectures, practical, laboratory,
                                                         reporting_form))
-    conn.commit()
+    con.commit()
 
 
 def find_discipline_by_code(code):
@@ -35,12 +34,11 @@ def find_discipline_by_name(name):
     cursor.execute('''SELECT * FROM Дисциплины WHERE Наименование = ?''', (name,))
     return cursor.fetchall()
 
-# Поиск по другим полям и для удаления и редактирования записей
+# Поиск по другим полям для удаления и редактирования
 
 
 add_discipline(1, 'Математика', 'Математика', 20, 30, 10, 'Экзамен')
 result = find_discipline_by_code(1)
 print(result)
 
-# Закрытие соединения с базой данных
-conn.close()
+con.close()
