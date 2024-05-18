56ede1b7d62af062e77531a2fca7c8aa492e3984
OnLy310 <eliseev881@gmail.com>
2024-05-18 17:56:46 +0300

    Full

---
+++ OS/part 2.py
@@ -0,0 +1,28 @@
+import os
+
+# text_file = open("text.txt", "w")
+#
+# text_file.write("Это текстовый файл")
+# text_file.close()
+
+# os.rename("text.txt", "renamed-text.txt")
+
+# os.replace("renamed-text.txt", "folder/renamed-text.txt")
+
+# print("Все папки и файлы:", os.listdir())
+
+# for dirpath, dirnames, filenames in os.walk("."):
+#     for dirname in dirnames:
+#         print("Каталог:", os.path.join(dirpath, dirname))
+#     for filename in filenames:
+#         print("Файл:", os.path.join(dirpath, filename))
+
+# os.remove("folder/renamed-text.txt")
+#
+# os.rmdir("folder")
+#
+# os.removedirs("nested1/nested2/nested3")
+
+# print(os.stat("text.txt"))
+
+# print("Размер файла:", os.stat("text.txt").st_size)
--- PZ_15/PZ_15.py
+++ PZ_15/PZ_15.py
@@ -4,42 +4,71 @@
 
 import sqlite3 as sq
 
-with sq.connect('учебный_план.db') as con:
+with sq.connect('syllabus.db') as con:
     cur = con.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS Дисциплины (
-                  Код INTEGER PRIMARY KEY,
-                  Наименование TEXT,
-                  Специальность TEXT,
-                  Лекции INTEGER,
-                  Практические INTEGER,
-                  Лабораторные INTEGER,
-                  Форма_отчетности TEXT)''')
+    con.execute("DROP TABLE IF EXISTS Disciplines")
+    cur.execute('''CREATE TABLE IF NOT EXISTS Disciplines (
+                  Code INTEGER PRIMARY KEY,
+                  Name TEXT,
+                  Speciality TEXT,
+                  Lectures INTEGER,
+                  Practical INTEGER,
+                  Laboratory INTEGER,
+                  Reporting_form TEXT
+                  )''')
     con.commit()
 
 
 def add_discipline(code, name, speciality, lectures, practical, laboratory, reporting_form):
-    cur.execute('''INSERT INTO Дисциплины (Код, Наименование, Специальность, Лекции, Практические, Лабораторные,
-    Форма_отчетности)
-    VALUES (?, ?, ?, ?, ?, ?, ?)''',
-                (code, name, speciality, lectures, practical, laboratory,
-                 reporting_form))
+    cur.execute('''
+    INSERT INTO disciplines (Code, Name, Speciality, Lectures, Practical, Laboratory,
+    Reporting_form)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', (code, name, speciality, lectures, practical, laboratory, reporting_form))
     con.commit()
 
 
-def find_discipline_by_code(code):
-    cur.execute('''SELECT * FROM Дисциплины WHERE Код = ?''', (code,))
-    return cur.fetchall()
+def findDisciplineByCode(code):
+    cur = con.cursor()
+    cur.execute('''SELECT * FROM Disciplines WHERE Code = ?''', (code,))
+    result = cur.fetchall()
+    return result
 
 
-def find_discipline_by_name(name):
-    cur.execute('''SELECT * FROM Дисциплины WHERE Наименование = ?''', (name,))
-    return cur.fetchall()
+def findDisciplineByName(name):
+    cur = con.cursor()
+    cur.execute('''SELECT * FROM Disciplines WHERE Name = ?''', (name,))
+    result = cur.fetchall()
+    return result
+
+def deleteByNumber(disciplines_id):
+    con = sq.connect("syllabus.db")
+    cur = con.cursor()
+    cur.execute('''DELETE FROM Disciplines WHERE disciplines_id=?''', (disciplines_id, ))
+    con.commit()
+    con.close()
 
 # Поиск по другим полям для удаления и редактирования
 
 
 add_discipline(1, 'Математика', 'Математика', 20, 30, 10, 'Экзамен')
-result = find_discipline_by_code(1)
-print(result)
+add_discipline(2, 'Русский язык', 'Русская филология', 25, 35, 15, 'Экзамен')
+add_discipline(3, 'История', 'История', 30, 45, 25, 'Экзамен')
+add_discipline(4, 'Химия', 'Химия', 50, 37, 28, 'Экзамен')
+add_discipline(5, 'Физика', 'Физика', 47, 52, 15, 'Экзамен')
+add_discipline(6, 'География', 'География', 15, 25, 5, 'Экзамен')
+add_discipline(7, 'Литература', 'Литература', 55, 45, 25, 'Экзамен')
+add_discipline(8, 'Биология', 'Биология', 20, 25, 15, 'Экзамен')
+add_discipline(9, 'Физическая культура', 'Физическая культура', 5, 8, 7, 'Экзамен')
+add_discipline(10, 'Английский язык', 'Английский язык', 35, 45, 25, 'Экзамен')
+
+def getDisciplines():
+    con = sq.connect("syllabus.db")
+    c = con.cursor()
+    c.execute('SELECT * FROM disciplines')
+    return c.fetchall()
+
+for disciplanes in getDisciplines():
+    print(disciplanes)
 
 con.close()
